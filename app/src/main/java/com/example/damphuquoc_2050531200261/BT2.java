b462c1bf4af5daf0bf20eb32dbc8b20271d71dfa
Fuquoc <damphuquoc1210@gmail.com>
2022-08-30 16:18:52 +0700

    first Commit

---
+++ app/src/main/java/com/example/damphuquoc_2050531200261/BT2.java
@@ -0,0 +1,14 @@
+package com.example.damphuquoc_2050531200261;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BT2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bt2);
+    }
+}
---
+++ app/src/main/java/com/example/damphuquoc_2050531200261/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.damphuquoc_2050531200261;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bt2);
+    }
+}
